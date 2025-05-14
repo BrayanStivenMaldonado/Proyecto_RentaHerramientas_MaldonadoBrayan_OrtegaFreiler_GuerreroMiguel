4b310f9dc8f92e2c4eeeb024ec43fb5a02f5d353
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2025-05-14 10:31:19 -0500

    feat: :tada: Initiation commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Renta de Herraminetas MSF
+
+Este proyecto tiene como objetivo desarrollar una Plataforma de Renta de Herramientas y Equipos de Construcción, donde los proveedores puedan registrar su inventario y los clientes puedan alquilar herramientas, programar entregas y gestionar pagos de forma eficiente.
---
+++ src/main/java/com/alquiler/alquiler_app/AlquilerAppApplication.java
@@ -0,0 +1,13 @@
+package com.alquiler.alquiler_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerAppApplication.class, args);
+	}
+
+}
