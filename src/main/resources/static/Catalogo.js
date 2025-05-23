7779de48639944749a3d37697438e88068948a18
FreilerOrtega <aleiroestupinan1999@gmail.com>
2025-05-23 00:59:21 -0500

    feat: :lipstick:  Add views admin

--- src/main/resources/static/Catalogo.js
+++ src/main/resources/static/Catalogo.js
@@ -1,27 +1,36 @@
-const token = localStorage.getItem("token");
 
-fetch('http://localhost:8080/api/tools', {
-    headers: {
-        'Authorization': 'Bearer ' + token
-    }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const productosContainer = document.querySelector('.productos');
+
+  fetch('http://localhost:8080/api/tools')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo obtener las herramientas');
+      }
+      return response.json();
+    })
+    .then(data => {
+      productosContainer.innerHTML = ''; // Limpiar por si hay contenido anterior
+      data.forEach(tool => {
+        const producto = document.createElement('div');
+        producto.classList.add('producto');
+
+        producto.innerHTML = `
+          <div>
+            <a href="../Herramientas/Herramientas.html" class="Link_Herramientas">
+              <img src="../media/${tool.image}" alt="${tool.toolName}">
+            </a>
+          </div>
+          <div>${tool.toolName}</div>
+          <div>$${tool.rentalPrice} por día</div>
+        `;
+
+        productosContainer.appendChild(producto);
+      });
+    })
+    .catch(error => {
+      console.error('Error al cargar herramientas:', error);
+      productosContainer.innerHTML = '<p>Error al cargar herramientas.</p>';
+    });
 });
-
-function Mostrar_Notificaciones() {
-    const Mensajes = document.getElementById("Mensajes");
-    if (Mensajes.style.display == "none") {
-        Mensajes.style.display = "flex";
-    } else {
-        Mensajes.style.display = "none";
-    }
-}
-
-function toggleMenu() {
-    const menu = document.getElementById("navMenu");
-    const mesanje = document.getElementById("Mensajes");
-
-    menu.classList.toggle("active");
-
-    if (!menu.classList.contains("active")) {
-        mesanje.style.display = "none";
-    }
-}
