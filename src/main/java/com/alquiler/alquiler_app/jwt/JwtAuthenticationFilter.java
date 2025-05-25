5924730316615cf5f43de38210f3f0273251300e
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2025-05-24 21:32:08 -0500

    feat: :sparkles: Permissions for users

--- src/main/java/com/alquiler/alquiler_app/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/alquiler/alquiler_app/jwt/JwtAuthenticationFilter.java
@@ -55,8 +55,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-
-        filterChain.doFilter(request, response);   
     }
 
     private String getTokenFromRequest(HttpServletRequest request) {
